b6b087f999ea8876485f8a2298224cbdc7f70d70
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-03-16 19:59:59 +0100

    Update app.ts y app.html (Usando date)

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NgModule} from '@angular/core'
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //imports: [RouterOutlet],
+  imports:[CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('front-end');
+  protected readonly title = signal('Este es el titulo que yo quiero');
+
+  manejarClick(){
+    // Hacer lo que yo quiera
+  }
+  duplicarNumero(valor:number): number{
+    return valor*2;
+  }
+
+  pelicula = {
+    titulo : 'Spider-man',
+    fechaLanzamiento: new Date()
+  };
 }
