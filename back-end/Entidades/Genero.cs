7c7cc4dfd27024d1072454d43b5a00978fd1f8c6
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-03-19 19:46:21 +0100

    Validaciones personalizadas realizadas

--- back-end/Entidades/Genero.cs
+++ back-end/Entidades/Genero.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using back_end.Validaciones;
+using System.ComponentModel.DataAnnotations;
 
 namespace back_end.Entidades
 {
-    public class Genero
+    public class Genero: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="El campo {0} es requerido")]
         [StringLength(maximumLength:10)]
+        //[PrimerLetraMayuscula]
         public string Nombre { get; set; }
         [Range(18,200)]
         public int Edad { get; set; }
@@ -14,5 +16,19 @@ namespace back_end.Entidades
         public string TarjetaDeCredito { get; set; }
         [Url]
         public string URL { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primer letra debe ser mayuscula",
+                        new string[] {nameof(Nombre)});
+                }
+            }
+        }
     }
 }
--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9147ba285ebed1e5bbfadad4b3a0432a3067d11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8483e890e34baddfaa5f8759351dfd782c00ec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
