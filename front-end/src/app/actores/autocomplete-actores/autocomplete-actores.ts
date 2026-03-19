7b90ad85377cd9fbcb4343d9e7b33917e6f98c08
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-03-19 16:19:51 +0100

    2ºParte 2/2 selector de autores realizado

--- front-end/src/app/actores/autocomplete-actores/autocomplete-actores.ts
+++ front-end/src/app/actores/autocomplete-actores/autocomplete-actores.ts
@@ -13,18 +13,28 @@ import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 export class AutocompleteActores implements OnInit{
   constructor(){}
 
-  control: FormControl;
+  control: FormControl = new FormControl();
+
   actores = [
     {nombre: 'Will Smith', foto: 'https://tse2.mm.bing.net/th/id/OIP.JNhieu6PaMircqOMyeNhVgHaJy?pid=Api&P=0&h=180'},
     {nombre: 'Eddy Murphy', foto: 'https://tse1.mm.bing.net/th/id/OIP.rhKXPqwaLFMKl_Yx-mK3QAHaKZ?pid=Api&P=0&h=180'},
     {nombre: 'Tom Hanks', foto: 'https://tse2.mm.bing.net/th/id/OIP.LaiGMyID4gVz8AZegmU5rwHaE7?pid=Api&P=0&h=180'}
   ]
 
+  actoresOriginal = this.actores;
+
+  actoresSeleccionados = [];
+
   ngOnInit(): void {
-    
+    this.control.valueChanges.subscribe(valor => {
+      this.actores = this.actoresOriginal;
+      this.actores = this.actores.filter(actor => actor.nombre.indexOf(valor) !== -1)
+    })
   }
 
   optionSelected(event: MatAutocompleteSelectedEvent){
     console.log(event.option.value);
+    this.actoresSeleccionados.push(event.option.value);
+    this.control.patchValue('');
   }
 }
