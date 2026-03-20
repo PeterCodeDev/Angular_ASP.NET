5a5139e269026f42c6026fa526f7d2893ea38631
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-03-20 14:23:56 +0100

    Injectable de los servicios

---
+++ front-end/src/app/generos/generos.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { generoDTO } from './genero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GenerosService {
+  constructor(){
+
+  }
+  public obtenerTodos(): generoDTO[]{
+    return [{id:1, nombre:'Drama'}];
+  }
+
+}
--- front-end/src/app/generos/indice-generos/indice-generos.ts
+++ front-end/src/app/generos/indice-generos/indice-generos.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatAnchor } from "@angular/material/button";
 import { RouterLink } from "@angular/router";
+import {GenerosService} from '../generos.service'
 
 @Component({
   selector: 'app-indice-generos',
@@ -8,4 +9,13 @@ import { RouterLink } from "@angular/router";
   templateUrl: './indice-generos.html',
   styleUrl: './indice-generos.css',
 })
-export class IndiceGeneros {}
+export class IndiceGeneros implements OnInit{
+  constructor(private generosService: GenerosService){
+
+  }
+
+  ngOnInit(): void {
+    const generos = this.generosService.obtenerTodos();
+    console.log(generos);
+  }
+}
