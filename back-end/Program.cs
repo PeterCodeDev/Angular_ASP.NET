b972e35fb33813e8d29c18be1f5b39ffa2357434
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-03-20 16:44:29 +0100

    Conexion entre back-end y front-end realizada

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -6,6 +6,14 @@ using back_end.Filtros;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    var frontend_url = builder.Configuration.GetValue<string>("frontend_url");
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(frontend_url).AllowAnyMethod().AllowAnyHeader();
+    });
+});
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
 builder.Services.AddControllers(options =>
 {
@@ -15,7 +23,7 @@ builder.Services.AddControllers(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PeliculasAPI", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "back_end", Version = "v1" });
 });
 
 var app = builder.Build();
@@ -34,6 +42,28 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseCors();
+
+app.Use(async (context, next) =>
+{
+    using (var swapStream = new MemoryStream())
+    {
+        var respuestaOriginal = context.Response.Body;
+        context.Response.Body = swapStream;
+
+        await next.Invoke();
+
+        swapStream.Seek(0, SeekOrigin.Begin);
+        string respuesta = new StreamReader(swapStream).ReadToEnd();
+        swapStream.Seek(0, SeekOrigin.Begin);
+
+        await swapStream.CopyToAsync(respuestaOriginal);
+        context.Response.Body = respuestaOriginal;
+
+        app.Logger.LogInformation(respuesta);
+    }
+});
 
 app.UseAuthentication();
 
