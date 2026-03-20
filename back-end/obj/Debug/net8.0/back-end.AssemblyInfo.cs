ad5317578cd3f4550acdc9e7d1f739d93d4ac060
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-03-20 12:27:02 +0100

    Middelware realizado

--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed604f47fc5af0548a535f8e84419e84d48e89a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30b4a62a88a2ade24581a67a41c090454c77a60")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
